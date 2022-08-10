e91ccd8d09ace94b8796ae7fc68250475cfa7514
bibekshhh <bibekrock2019@gmail.com>
2022-08-10 16:27:59 +0545

    Initiial commit

---
+++ app.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const router = require('express').Router();
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const userSchema = require('./model/db')
+
+const app = express();
+
+const database = () => {
+    const connectionParams = {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    }
+
+    try {
+        mongoose.connect('mongodb+srv://bibekshah:bibek123@cluster0.dsfsc1m.mongodb.net/?retryWrites=true&w=majority', connectionParams);
+        console.log("Databse connected successfully!")
+    } catch (err) {
+        console.log(error)
+        console.log('Database connection failed!')
+    }
+}
+
+database();
+
+//set the view engine to ejs
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Actual routing
+app.get('/', (req, res) => {
+    res.render('index')
+});
+
+app.post('/', (req, res) => {
+    var newUser = new userSchema();
+
+    newUser.name = req.body.name;
+    newUser.email = req.body.email;
+    newUser.password = req.body.password;
+
+    newUser.save((err, data) => {
+        if (err) {
+            console.log('Data not inserted: ' + err)
+        } else {
+            res.send('Data inserted successfully')
+        }
+    });
+
+    res.redirect('/')
+});
+
+
+app.listen(5050, () => {
+    console.log("Listening on port 5050");
+})
